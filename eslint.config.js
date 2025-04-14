c5d7f717f9243565dc565a747695ed4d4505576f
Yu-Kashiro <poyonnnnnn@gmail.com>
2025-04-14 22:14:17 +0900

    ESLintを設定

---
+++ eslint.config.js
@@ -0,0 +1,22 @@
+import globals from "globals";
+import { defineConfig } from "eslint/config";
+import js from "@eslint/js";
+
+export default defineConfig([
+	{
+		files: ["**/*.js"],
+		plugins: {
+			js,
+		},
+		extends: ["js/recommended"],
+		rules: {
+			"no-unused-vars": "warn",
+			"no-undef": "warn",
+		},
+		languageOptions: {
+			globals: {
+				...globals.browser,
+			},
+		},
+	},
+]);
