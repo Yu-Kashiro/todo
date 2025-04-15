e8bcd5bac79343d351f702c8a3541b5a53ab51f3
Yu-Kashiro <poyonnnnnn@gmail.com>
2025-04-15 20:53:30 +0900

    prettierの導入

--- eslint.config.js
+++ eslint.config.js
@@ -3,20 +3,20 @@ import { defineConfig } from "eslint/config";
 import js from "@eslint/js";
 
 export default defineConfig([
-	{
-		files: ["**/*.js"],
-		plugins: {
-			js,
-		},
-		extends: ["js/recommended"],
-		rules: {
-			"no-unused-vars": "warn",
-			"no-undef": "warn",
-		},
-		languageOptions: {
-			globals: {
-				...globals.browser,
-			},
-		},
-	},
+  {
+    files: ["**/*.js"],
+    plugins: {
+      js,
+    },
+    extends: ["js/recommended"],
+    rules: {
+      "no-unused-vars": "warn",
+      "no-undef": "warn",
+    },
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+      },
+    },
+  },
 ]);
